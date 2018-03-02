6cc885f038faef392ecfad8c2ec721db5f58aee1
Paul Joannon <hello@pauljoannon.com>
2018-03-02 12:06:52 +0100

    use InkStory in example scene, not with a singleton

--- main.gd
+++ main.gd
@@ -2,21 +2,18 @@ extends Node
 
 func _ready():
     set_process(true)
-    InkStory.connect("InkContinued", self, "_on_story_continued")
-    InkStory.connect("InkChoices", self, "_on_choices")
-    InkStory.LoadStory("ink/example.json")
-    InkStory.Continue();
+    $InkStory.LoadStory("ink/example.json")
+    $InkStory.Continue();
     
 func _process(delta):
     if Input.is_action_just_pressed("click_left"):
         if $DialogBox.call("Next"):
-            if InkStory.CanContinue:
-                InkStory.Continue()
+            if $InkStory.CanContinue:
+                $InkStory.Continue()
+            elif $InkStory.HasChoices:
+                $InkStory.ChooseChoiceIndex(0)
             else:
                 $DialogBox.call("ChangeText", "")
 
-func _on_choices(choices):
-    pass
-
-func _on_story_continued(text):
+func _on_story_continued(text, tags):
     $DialogBox.call("ChangeText", text)
