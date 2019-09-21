d7c0215445dceab8cefa5558a3233ffb65b23e4c
Paul Joannon <hello@pauljoannon.com>
2019-09-21 21:27:58 +0200

    can directly import ink files
    .ink files will be compiled to .json if the inklecate path is set correctly in the project settings

--- addons/paulloz.ink/PaullozDotInk.cs
+++ addons/paulloz.ink/PaullozDotInk.cs
@@ -1,10 +1,12 @@
 #if TOOLS
 using Godot;
+using Godot.Collections;
 using System;
 
 [Tool]
 public class PaullozDotInk : EditorPlugin
 {
+    private Dictionary settings = new Dictionary() { {"inklecate_path", "---"} };
     private const String addonBasePath = "res://addons/paulloz.ink";
 
     private NodePath customTypeScriptPath = $"{addonBasePath}/InkStory.cs";
@@ -18,23 +20,49 @@ public class PaullozDotInk : EditorPlugin
 
     public override void _EnterTree()
     {
+        // Settings
+        foreach (String key in settings.Keys)
+        {
+            String property_name = $"ink/{key}";
+            if (!ProjectSettings.HasSetting(property_name))
+            {
+                ProjectSettings.SetSetting(property_name, settings[key]);
+                ProjectSettings.SetInitialValue(property_name, settings[key]);
+            }
+        }
+        ProjectSettings.Save();
+
+        // Resources
         importPlugin = (ResourceLoader.Load(importPluginScriptPath) as GDScript).New() as EditorImportPlugin;
         AddImportPlugin(importPlugin);
 
+        // Custom types
         AddCustomType("Ink Story", "Node", ResourceLoader.Load(customTypeScriptPath) as Script, ResourceLoader.Load(customTypeIconPath) as Texture);
 
+        // Editor
         dock = (ResourceLoader.Load(dockScene) as PackedScene).Instance() as Control;
         AddControlToBottomPanel(dock, "Ink");
     }
 
     public override void _ExitTree()
     {
+        // Editor
         RemoveControlFromBottomPanel(dock);
         dock.Free();
 
+        // Custom types
         RemoveCustomType("Ink Story");
 
+        // Resources
         RemoveImportPlugin(importPlugin);
+
+        // Settings
+        foreach (String key in settings.Keys)
+        {
+            String property_name = $"ink/{key}";
+            if (ProjectSettings.HasSetting(property_name))
+                ProjectSettings.SetSetting(property_name, null);
+        }
     }
 }
 #endif
--- addons/paulloz.ink/import_ink.gd
+++ addons/paulloz.ink/import_ink.gd
@@ -8,7 +8,7 @@ func get_visible_name():
     return "JSON ink story";
 
 func get_recognized_extensions():
-    return [ "json" ];
+    return [ "json", "ink" ];
 
 func get_save_extension():
     return "res";
@@ -23,12 +23,33 @@ func get_preset_count():
     return 0
 
 func import(source_file, save_path, options, r_platform_variants, r_gen_files):
-    var file = File.new()
-    var err = file.open(source_file, File.READ)
-    if err != OK:
-        return err
-    var raw_content = file.get_as_text()
-    file.close()
+    match source_file.split(".")[-1].to_lower():
+        "ink":
+            return import_from_ink(source_file, save_path)
+        "json":
+            return import_from_json(source_file, save_path)
+
+func import_from_ink(source_file, save_path):
+    if ProjectSettings.has_setting("ink/inklecate_path"):
+        var inklecate = ProjectSettings.get_setting("ink/inklecate_path")
+        if inklecate != "---":
+            var new_file = "%d.json" % int(randf() * 100000)
+
+            var err = OS.execute(inklecate, [
+                "-o", "%s/%s" % [OS.get_user_data_dir(), new_file],
+                ProjectSettings.globalize_path(source_file)
+            ], true)
+
+            new_file = "user://%s" % new_file
+            if !File.new().file_exists(new_file):
+                return ERR_FILE_UNRECOGNIZED
+            var ret = import_from_json(new_file, save_path)
+
+            Directory.new().remove(new_file)
+            return ret
+
+func import_from_json(source_file, save_path):
+    var raw_content = get_source_file_content(source_file)
 
     var parsed_content = parse_json(raw_content)
     if !parsed_content.has("inkVersion"):
@@ -36,4 +57,16 @@ func import(source_file, save_path, options, r_platform_variants, r_gen_files):
 
     var resource = TextFile.new()
     resource.set_meta("content", raw_content);
+
     return ResourceSaver.save("%s.%s" % [save_path, get_save_extension()], resource)
+
+func get_source_file_content(source_file):
+    var file = File.new()
+    var err = file.open(source_file, File.READ)
+    if err != OK:
+        return err
+
+    var raw_content = file.get_as_text()
+
+    file.close()
+    return raw_content
