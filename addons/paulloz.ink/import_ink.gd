a179583b913f264bdb58cbe3cffc20a00694bd3e
Paul Joannon <hello@pauljoannon.com>
2020-06-14 00:54:09 +0200

    add a 'compress' import option
    this new option defaults to true

--- addons/paulloz.ink/import_ink.gd
+++ addons/paulloz.ink/import_ink.gd
@@ -17,7 +17,9 @@ func get_resource_type():
     return "Resource";
 
 func get_import_options(preset):
-    return []
+    return [
+        {"name": "compress", "default_value": true}
+    ]
 
 func get_preset_count():
     return 0
@@ -25,11 +27,11 @@ func get_preset_count():
 func import(source_file, save_path, options, r_platform_variants, r_gen_files):
     match source_file.split(".")[-1].to_lower():
         "ink":
-            return import_from_ink(source_file, save_path)
+            return import_from_ink(source_file, save_path, options)
         "json":
-            return import_from_json(source_file, save_path)
+            return import_from_json(source_file, save_path, options)
 
-func import_from_ink(source_file, save_path):
+func import_from_ink(source_file, save_path, options):
     var setting = "ink/inklecate_path"
     if ProjectSettings.has_setting(setting) and ProjectSettings.property_can_revert(setting):
         var inklecate = ProjectSettings.get_setting(setting)
@@ -52,7 +54,7 @@ func import_from_ink(source_file, save_path):
         new_file = "user://%s" % new_file
         if !File.new().file_exists(new_file):
             return ERR_FILE_UNRECOGNIZED
-        var ret = import_from_json(new_file, save_path)
+        var ret = import_from_json(new_file, save_path, options)
 
         Directory.new().remove(new_file)
         return ret
@@ -60,7 +62,7 @@ func import_from_ink(source_file, save_path):
         printerr("Please update inklecate_path setting to be able to compile ink files.")
         return ERR_COMPILATION_FAILED
 
-func import_from_json(source_file, save_path):
+func import_from_json(source_file, save_path, options):
     var raw_content = get_source_file_content(source_file)
 
     var parsed_content = parse_json(raw_content)
@@ -70,7 +72,8 @@ func import_from_json(source_file, save_path):
     var resource = Resource.new()
     resource.set_meta("content", raw_content);
 
-    return ResourceSaver.save("%s.%s" % [save_path, get_save_extension()], resource)
+    var flags = ResourceSaver.FLAG_COMPRESS if options["compress"] else 0
+    return ResourceSaver.save("%s.%s" % [save_path, get_save_extension()], resource, flags)
 
 func get_source_file_content(source_file):
     var file = File.new()
