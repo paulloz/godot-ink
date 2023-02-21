64099760e5813cb286922371d7c68d54c2e1ee09
Paul Joannon <hello@pauljoannon.com>
2023-02-21 18:40:12 +0100

    Add interop get methods in `InkChoice`

---
+++ addons/GodotInk/Src/InkChoice.Interop.cs
@@ -0,0 +1,38 @@
+#nullable enable
+
+using System.ComponentModel;
+
+namespace GodotInk;
+
+public partial class InkChoice
+{
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="CanContinue" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public string GetText() => Text;
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="CanContinue" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public string GetPathStringOnChoice() => PathStringOnChoice;
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="CanContinue" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public string GetSourcePath() => SourcePath;
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="CanContinue" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public int GetIndex() => Index;
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="CanContinue" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public Godot.Collections.Array<string> GetTags() => new(Tags);
+}
