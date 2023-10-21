1eb7e1d79e90036226732d78acbc25052b93cc56
Paul Joannon <hello@pauljoannon.com>
2023-10-21 17:14:40 +0200

    Fix mixed indentation in plugin.gd

--- addons/GodotInk/plugin.gd
+++ addons/GodotInk/plugin.gd
@@ -44,10 +44,12 @@ func _notification(what : int) -> void:
 		if _importer != null:
 			return
 		get_editor_interface().set_plugin_enabled("GodotInk", false)
-		printerr("GodotInk could not be loaded.\n" +
-				 "Make sure your .csproj file references GodotInk.props, and rebuild the solution.\n" +
-				 "See the installation guide for more information: https://github.com/paulloz/godot-ink/wiki\n" +
-				 "If you think this is not an issue on your end, please open a bug report: https://github.com/paulloz/godot-ink/issues/new/choose")
+		printerr(
+			"GodotInk could not be loaded.\n" +
+			"Make sure your .csproj file references GodotInk.props, and rebuild the solution.\n" +
+			"See the installation guide for more information: https://github.com/paulloz/godot-ink/wiki\n" +
+			"If you think this is not an issue on your end, please open a bug report: https://github.com/paulloz/godot-ink/issues/new/choose"
+		)
 
 
 func _when_resources_reimported(resources : PackedStringArray) -> void:
