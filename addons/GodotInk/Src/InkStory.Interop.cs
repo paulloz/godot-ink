43cbffc5cb52773797648c3a8ef69731d930148e
Paul Joannon <hello@pauljoannon.com>
2024-10-30 23:14:49 +0100

    Add some GDScript compatibility methods
    [CHANGELOG]
    Add GDScript compatibility methods for the following members: `GlobalTags`, `TagsForContentAtPath(string)`, `CurrentWarnings`, `CurrentErrors`

--- addons/GodotInk/Src/InkStory.Interop.cs
+++ addons/GodotInk/Src/InkStory.Interop.cs
@@ -5,6 +5,8 @@ using System;
 using System.ComponentModel;
 using System.Linq;
 
+using InkChoiceGArray = Godot.Collections.Array<GodotInk.InkChoice>;
+using StringGArray = Godot.Collections.Array<string>;
 using VariantGArray = Godot.Collections.Array<Godot.Variant>;
 
 namespace GodotInk;
@@ -22,19 +24,43 @@ public partial class InkStory
     /// This method is here for GDScript compatibility. Use <see cref="CurrentChoices" /> instead.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public Godot.Collections.Array<InkChoice> GetCurrentChoices() => new(CurrentChoices);
+    public InkChoiceGArray GetCurrentChoices() => new(CurrentChoices);
 
     /// <summary>
     /// This method is here for GDScript compatibility. Use <see cref="CurrentTags" /> instead.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public Godot.Collections.Array<string> GetCurrentTags() => new(CurrentTags);
+    public StringGArray GetCurrentTags() => new(CurrentTags);
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="GlobalTags" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public StringGArray GetGlobalTags() => new(GlobalTags);
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="TagsForContentAtPath" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public StringGArray GetTagsForContentAtPath(string path) => new(TagsForContentAtPath(path));
 
     /// <summary>
     /// This method is here for GDScript compatibility. Use <see cref="CurrentText" /> instead.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
     public string GetCurrentText() => CurrentText;
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="CurrentWarnings" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public StringGArray GetCurrentWarnings() => new(CurrentWarnings);
+
+    /// <summary>
+    /// This method is here for GDScript compatibility. Use <see cref="CurrentErrors" /> instead.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public StringGArray GetCurrentErrors() => new(CurrentErrors);
 #pragma warning restore IDE0022
 
     [EditorBrowsable(EditorBrowsableState.Never)]
@@ -60,7 +86,6 @@ public partial class InkStory
     {
         ChoosePathString(path, resetCallstack, arguments.ToArray());
     }
-
 
     /// <summary>
     /// Evaluates a function defined in ink.
